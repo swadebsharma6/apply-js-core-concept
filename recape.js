4ea732856fc2e8b1edba83f87bca8bcf0c62155e
swadebsharma6 <swadebriva2012@gmail.com>
2023-08-07 04:06:27 -0700

    first commit

---
+++ recape.js
@@ -0,0 +1,21 @@
+var myName = 'Swadeb Sharma';
+
+
+// math operation: +, -, *, /
+// shorthand: +-, --, +-, /-;
+
+// Array
+var friends = ['abul', 'kabul', 'Sabul', 'Tabul'];
+
+var thirdFriend = friends[2];
+friends[3]= 'Bagher baccha Tabu'
+console.log(thirdFriend);
+
+// condition
+//  <, >, <==, >==, !==, 
+if(friends.length > 2){
+    console.log('I have lot of friends')
+}
+else{
+    console.log('i  have no friends')
+}
